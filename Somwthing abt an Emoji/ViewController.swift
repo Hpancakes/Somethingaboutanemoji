2ccdcd0347bfc3c9c07a41461e1a85909cde1cce
Taslot <jluis.larav@gmail.com>
2017-06-28 20:08:35 -0500

    Ya con caritas

--- Somwthing abt an Emoji/ViewController.swift
+++ Somwthing abt an Emoji/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text =  "😖"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
