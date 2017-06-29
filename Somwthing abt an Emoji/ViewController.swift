bbbe9cef9ede3dee7e5532bf6874134e4feeef60
Taslot <jluis.larav@gmail.com>
2017-06-28 23:04:27 -0500

    Ya con array y funcionando

--- Somwthing abt an Emoji/ViewController.swift
+++ Somwthing abt an Emoji/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
-    
+    var emojiArray = ["😖", "😑", "🤡", "😡", "💩", "🤣", "😵", "🤑", "😤", "🤓", "🤤"]
+        
+        
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,13 +26,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return emojiArray.count
     
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print(indexPath.row)
+        
         let cell = UITableViewCell()
-        cell.textLabel?.text =  "😖"
+        cell.textLabel?.text =  emojiArray[indexPath.row]
         return cell
     }
 
