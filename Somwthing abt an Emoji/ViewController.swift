7eeb49be44a3a59ff62ff4bb445c05d2205f70aa
Taslot <jluis.larav@gmail.com>
2017-06-28 19:29:46 -0500

    Initial Commit

---
+++ Somwthing abt an Emoji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Somwthing abt an Emoji
+//
+//  Created by JoseLuis Lara VIllanueva on 6/28/17.
+//  Copyright © 2017 Pachuca Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
